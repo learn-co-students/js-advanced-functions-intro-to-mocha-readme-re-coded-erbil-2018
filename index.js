fe9f51d5db4b20213b912619b2db511bc806a8e8
Bestun Gardi <bestun95@yahoo.com>
2018-09-06 07:00:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+let result ='result';
+let hi ='hi';
+let Name='name';
+let email ='email';
+let flavor ='flavor';
+let string='flavor';
+let number='number';
+ function theTruth(result) {
+  return true;
+}
+theTruth(result);
+ function sayHey(hi) {
+  return 'Hey!';
+}
+ function sayHeyFriend(Name,email) {
+  return `Hey, ${Name}!`;
+}
+ function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
+ function shouting(string) {
+  return string.toUpperCase();
+}
+ function roundDown(number) {
+  return Math.floor(number);
+}  
