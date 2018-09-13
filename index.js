772218973aaeadcbbc7c7d236f6ab3c9ed4cc08d
Mohammed Ameen <jegroamin@gmail.com>
2018-09-13 00:38:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function theTruth(){
+  return true;
+}
+function sayHey(){
+  return "Hey!";
+}
+function sayHeyFriend(name){
+  return `Hey, ${name}!`;
+}
+function favoriteIceCream(name){
+  return `I love ${name}`;
+}
+function shouting(name){
+  return name.toUpperCase();
+}
+function roundDown(name){
+  return Math.floor(name);
+}
